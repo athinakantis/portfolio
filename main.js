b72742004b2de15a6d7e8bf2e06f3a657b02c479
Athina <athina.kantis@gmail.com>
2024-10-26 16:16:01 +0300

    add typewriter effect

--- main.js
+++ main.js
@@ -9,6 +9,68 @@ const observer = new IntersectionObserver(entries => {
     })
 })
 
-
 progressBars.forEach((el) => observer.observe(el))
+
+
+const toTopBtn = document.querySelector('#toTopBtn')
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 195) {
+        toTopBtn.style.transform = `translateY(0)`;
+    } else {
+        toTopBtn.style.transform = `translateY(6rem)`;
+    };
+});
+
+toTopBtn.addEventListener('click', () => window.scroll(0, 0))
+
+
+
+function heroCode() {
+    document.createElement('p')
+    p.textContent = generateRandomCode()
+    console.log(generateRandomCode())
+}
+
+
+function generateRandomCode() {
+    return codeSnippets[Math.round(Math.random() * codeSnippets.length)]
+}
+
+
+const codeSnippets = [
+    'touch whoIsAthina.js',
+    'const favoriteAnimal = cats 😺',
+    `if (bored === true) {
+        paintSomething()
+    }`,
+    `while (coffeeCup === empty) {
+        athina.brewCoffee()
+    }`,
+    `if (gymday.includes(today)) {
+        athina.goGym()
+    }`,
+    `else {
+        athina.code()
+    }`
+]
+
+
+let typeWriterInterval;
+const command = document.querySelector('#command');
+
+function typeWriter(snippet) {
+    let index = 0;
+
+    typeWriterInterval = setInterval(() => {
+        command.textContent += snippet[index];
+        index++;
+
+        if (index === snippet.length) {
+            clearInterval(typeWriterInterval);
+        }
+    }, 100);
+}
+
+typeWriter('touch whoIsAthina.js');
 
